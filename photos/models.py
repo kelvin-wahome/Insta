9ac62f0bb88a46cba05088f16e974ffeb36c076e
blackrose <wahomekelving@gmail.com>
2019-03-04 10:26:42 +0300

    Created model Images

--- photos/models.py
+++ photos/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Images(models.Model):
+
+    title = models.CharField(max_length = 60)
+    description = models.CharField(max_length = 60)
+    image = models.ImageField(upload_to = 'articles/')
