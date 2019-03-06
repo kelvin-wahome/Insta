3f691e03a5ab4ca956886310e5f5bfea6fea57f4
blackrose <wahomekelving@gmail.com>
2019-03-06 13:19:48 +0300

    Added urls

--- photos/urls.py
+++ photos/urls.py
@@ -5,6 +5,11 @@ from . import views
 
 urlpatterns=[
     url('^$',views.index,name = 'index'),
+    url('^$',views.index,name = 'index'),
+    url(r'^search', views.search_image, name='search_image'),
+    url(r'^image/(\d+)',views.image,name ='image'),
+    url(r'location/(\d+)',views.filter_by_location,name ='location'),
+    url(r'category/(\d+)',views.filter_by_category,name ='category'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
