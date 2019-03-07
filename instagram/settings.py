d6fef36981dbe6e0660cec59b743c39ccfd127f3
blackrose <wahomekelving@gmail.com>
2019-03-07 18:44:24 +0300

    done

--- instagram/settings.py
+++ instagram/settings.py
@@ -108,14 +108,7 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'insta',
-        'USER': 'blackrose',
-        'PASSWORD':'alisak6996',
-    }
-}
+
 
 
 # Password validation
